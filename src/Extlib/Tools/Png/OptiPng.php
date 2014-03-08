62388f5e53603ed08e69562bee8c5c56d0c0fee6
Lukasz Ciolecki <lukasz.ciolecki@gmail.com>
2014-03-08 14:19:45 +0100

    Add resource images

--- src/Extlib/Tools/Png/OptiPng.php
+++ src/Extlib/Tools/Png/OptiPng.php
@@ -7,7 +7,7 @@ class OptiPng extends \PHPImageOptim\Tools\Png\OptiPng
 
     public function optimise()
     {
-        exec($this->binaryPath . ' -i0 -o7 -zm1-9 ' . $this->imagePath, $aOutput, $iResult);
+        exec($this->binaryPath . ' -i0 -o7' . $this->imagePath, $aOutput, $iResult);
         if ($iResult != 0) {
             throw new Exception('OPTIPNG was unable  to optimise image, result:' . $iResult . ' File: ' . $this->imagePath);
         }
